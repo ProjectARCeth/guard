8319a654210df012a13e1014a91e726dcfac9f15
Robin Deuber <rdeuber@student.ethz.ch>
2016-12-13 14:58:57 +0100

    Aenderungen

--- src/gridAnalyser.cpp
+++ src/gridAnalyser.cpp
@@ -1,5 +1,23 @@
-#include "gridAnalyser.hpp"
+#include "guard/inlcude/gridAnalyser/gridAnalyser.hpp"
 //Constructor
 gridAnalyser::gridAnalyser(const ros::NodeHandle &nh): nh_(nh)
 {
-
+	//First value of status_.
+	status_=0;
+	//Publsiher. 
+	//status_pub=nh_.advertise 
+	state_sub=nh_.subscribe("/state", 100, &gridAnalyser::getState, this);
+	//GridMap_sub=nh_.subscribe(...)
+}
+//Standart-Destructor
+gridAnalyser::~gridAnalyser(){}
+//Callback Function which processes incoming state
+void gridAnalyser::getState (const arc_msgs::State::ConstPtr& arc_state)
+{
+	//something is done
+}
+//Callback Function which processes incoming Gridmap
+//void gridAnalyser::getGridMap(const ...)
+{
+	//something ist done
+}
--- src/gridAnalyser_node.cpp
+++ src/gridAnalyser_node.cpp
@@ -1,2 +1,10 @@
 #include "ros/ros.h"
 #include "gridAnalyser.hpp"
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "gridAnalyser");
+	ros::Nodehandle node;
+	gridAnalyser gridAnalyser_object(node);
+	ros::spin();
+	return 0;
+}
